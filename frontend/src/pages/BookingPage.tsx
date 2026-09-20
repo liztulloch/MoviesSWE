f4d13e1b36f7624018ce550dd01c860fe7241806
Mukul Nimmagadda <mukuln321@mukuls-air.mynetworksettings.com>
2026-09-20 16:40:03 -0400

    Setup frontend config

---
+++ frontend/src/pages/BookingPage.tsx
@@ -0,0 +1,3 @@
+export default function BookingPage() {
+  return <h1>Booking page coming soon.</h1>;
+}
---
+++ frontend/src/pages/MovieDetailsPage.tsx
@@ -0,0 +1,3 @@
+export default function MovieDetailsPage() {
+  return <p>Movie details coming soon.</p>;
+}
